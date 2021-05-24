413452176d418e3424b1e2a87137311230f08980
Dominic Hains <contact.dominichains@gmail.com>
2021-05-23 23:33:45 -0400

    feat(about): added about text and translate

--- packages/client/src/components/About.js
+++ packages/client/src/components/About.js
@@ -12,29 +12,29 @@ const About = () => {
               <h4 className='card-title'>{t('guest.about.title')}</h4>
             </div>
             <div className='card-body'>
+              <p>{t('guest.about.p1')}</p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                ac nunc non magna vestibulum rutrum et quis elit. Duis imperdiet
-                magna molestie mauris commodo, in aliquam lacus sollicitudin.
-                Fusce sed sem non odio faucibus sodales. Praesent vitae
-                hendrerit nibh. Curabitur nec libero non nibh blandit mollis non
-                a tortor. Maecenas et dui sed lacus maximus pharetra. Nulla
-                hendrerit vitae enim eu egestas. Etiam non consequat metus. Cras
-                posuere sem eu sapien gravida tincidunt a sollicitudin erat.
-                Cras pellentesque feugiat odio non tincidunt. Cras at mi viverra
-                quam rutrum scelerisque. Nullam mauris velit, fringilla
-                facilisis turpis et, scelerisque vestibulum velit. <br /> <br />
-                Nullam id ex vel enim convallis cursus dignissim eu nisl.
-                Aliquam varius et diam vel venenatis. Nunc eros erat, aliquam a
-                quam lacinia, aliquet feugiat nunc. Ut euismod enim et lectus
-                consequat, a tincidunt purus blandit. Phasellus mi ligula,
-                efficitur vitae orci et, facilisis ultricies lorem. Nullam
-                mattis velit mauris, quis commodo nulla auctor a. Suspendisse
-                sit amet libero nisl. Maecenas accumsan sem vel tellus lacinia
-                vehicula. Mauris hendrerit felis vitae purus euismod, vitae
-                hendrerit lorem tincidunt. Cras dapibus nisi ut dapibus
-                faucibus. Etiam eleifend congue ipsum in convallis.
+                <b>{t('guest.about.features')}:</b>
               </p>
+              <p>{t('guest.about.patient')}</p>
+              <ul>
+                <li>{t('guest.about.patient1')}</li>
+                <li>{t('guest.about.patient2')}</li>
+                <li>{t('guest.about.patient3')}</li>
+              </ul>
+              <p>{t('guest.about.professional')}</p>
+              <ul>
+                <li>{t('guest.about.professional1')}</li>
+                <li>{t('guest.about.professional2')}</li>
+                <li>{t('guest.about.professional3')}</li>
+                <li>{t('guest.about.professional4')}</li>
+                <li>{t('guest.about.professional5')}</li>
+              </ul>
+              <p>{t('guest.about.researcher')}</p>
+              <ul>
+                <li>{t('guest.about.researcher1')}</li>
+                <li>{t('guest.about.researcher2')}</li>
+              </ul>
             </div>
           </div>
         </div>
