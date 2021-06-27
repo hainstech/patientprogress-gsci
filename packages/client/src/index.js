f7991079e30262fdfbf74b5f9939cbf864b0f101
hainsdominic <contact.dominichains@gmail.com>
2021-06-27 09:57:45 -0400

    feat(client): replace Loading... by the Spinner in the suspense

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import Spinner from './components/Spinner/Spinner';
 import App from './App';
 
 import './i18n';
 
 ReactDOM.render(
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<Spinner />}>
     <App />
   </Suspense>,
   document.getElementById('root')
