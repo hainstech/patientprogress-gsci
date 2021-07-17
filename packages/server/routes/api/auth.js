7b3c52cacc41cec38e0ecaba36c002725ee49d7e
hainsdominic <contact.dominichains@gmail.com>
2021-07-17 16:25:50 -0400

    fix(client): speed limiter
    Closes #54

--- packages/server/routes/api/auth.js
+++ packages/server/routes/api/auth.js
@@ -20,7 +20,7 @@ const client = new Redis();
 // Slow down requests
 const speedLimiter = slowDown({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  delayAfter: 100, // allow 100 requests per 15 minutes, then...
+  delayAfter: 50, // allow 100 requests per 15 minutes, then...
   delayMs: 500, // begin adding 500ms of delay per request above 100:
   // request # 101 is delayed by  500ms
   // request # 102 is delayed by 1000ms
